be141ba38514c4397b884c1c2bb99e7d8d9b6197
Irfan Rahman <Irfan@irfans-mbp.memphis.edu>
2017-03-11 22:04:54 -0600

    Initial Commit

---
+++ pokedex2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex2
+//
+//  Created by Irfan Rahman on 3/11/17.
+//  Copyright © 2017 Irfan Rahman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
